70f19640ce894c324810640a91d7e298f78827af
YoonseolLee <thisyoon97@gmail.com>
2024-04-09 17:17:43 +0900

    test: 블로그 글 수정 기능 테스트코드 작성

--- projects/simpleBlog/src/test/java/com/ryan/simpleBlog/controller/BlogApiControllerTest.java
+++ projects/simpleBlog/src/test/java/com/ryan/simpleBlog/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package com.ryan.simpleBlog.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ryan.simpleBlog.domain.Article;
 import com.ryan.simpleBlog.dto.AddArticleRequest;
+import com.ryan.simpleBlog.dto.UpdateArticleRequest;
 import com.ryan.simpleBlog.repository.BlogRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -152,5 +153,34 @@ class BlogApiControllerTest {
         // then
         List<Article> articles = blogRepository.findAll();
         Assertions.assertThat(articles).isEmpty();
+    }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle,
+                newContent);
+
+        // when
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+        Assertions.assertThat(article.getTitle()).isEqualTo(newTitle);
+        Assertions.assertThat(article.getContent()).isEqualTo(newContent);
     }
 }
