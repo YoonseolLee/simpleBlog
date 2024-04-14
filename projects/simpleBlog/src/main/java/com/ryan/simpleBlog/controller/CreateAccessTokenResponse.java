b88db86b6c999e4eab19e712c3043c936fa79da6
YoonseolLee <thisyoon97@gmail.com>
2024-04-14 23:24:25 +0900

    feat: 액세스/리프레시 토큰 응답/요청에 필요한 클래스 구현

---
+++ projects/simpleBlog/src/main/java/com/ryan/simpleBlog/controller/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package com.ryan.simpleBlog.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
