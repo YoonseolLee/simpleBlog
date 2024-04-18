8595ab966915a0353091673e06544aa8f1127463
YoonseolLee <thisyoon97@gmail.com>
2024-04-18 21:18:14 +0900

    feat: BlogService를 OAuth에 맞춰 재구현

--- projects/simpleBlog/src/main/java/com/ryan/simpleBlog/service/BlogService.java
+++ projects/simpleBlog/src/main/java/com/ryan/simpleBlog/service/BlogService.java
@@ -5,6 +5,7 @@ import com.ryan.simpleBlog.dto.AddArticleRequest;
 import com.ryan.simpleBlog.dto.UpdateArticleRequest;
 import com.ryan.simpleBlog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,17 +31,53 @@ public class BlogService {
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 
+//    public void delete(long id) {
+//        blogRepository.deleteById(id);
+//    }
+
+      // 트랜잭션 메서드
+//    @Transactional
+//    public Article update(long id, UpdateArticleRequest request) {
+//        Article article = blogRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+//
+//        article.update(request.getTitle(), request.getContent());
+//
+//        return article;
+//    }
+
+    /**
+     * OAuth
+     * 이제 글을 수정/삭제할 때 요청 헤더에 토큰을 전달하므로
+     * 사용자 자신이 작성한 글인지 검증할 수 있다.
+     * 따라서 본인 글이 아닌데 수정/삭제를 시도하는 경우에 예외를 발생시킨다.
+     */
+
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
-    @Transactional // 트랜잭션 메서드
+    @Transactional
     public Article update(long id, UpdateArticleRequest request) {
         Article article = blogRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
